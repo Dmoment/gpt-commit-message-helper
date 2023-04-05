42a8c96e87ec3a34beba5955b8840087a0af653e
Deepak Chauhan <rishudc119@gmail.com>
2023-04-05 14:59:31 +0530

    Add OpenAI support to commit_with_suggestion.sh script

--- commit_with_suggestion.sh
+++ commit_with_suggestion.sh
@@ -1,25 +1,19 @@
 #!/bin/sh
 
-# Create a temporary file in the system's temporary directory
-temp_file=$(mktemp)
+IFS=$'\n' read -d '' -r -a changed_files < <(git diff --cached --name-only)
 
-# Gather staged changes
-git diff --cached --name-status > "$temp_file"
-changes=$(cat "$temp_file")
+if [ ${#changed_files[@]} -gt 0 ]; then
+  commit_message=$(python3 /path/to/gpt_commit_message.py "${changed_files[@]}")
 
-# Remove the temporary file
-rm "$temp_file"
+  if [ -n "$commit_message" ]; then
+      echo "Suggested commit message: $commit_message"
+      echo "Press ENTER to use the suggested commit message, or type a custom one and press ENTER:"
+      read user_input
 
-commit_message=$(python3 gpt_commit_message.py "$changes")
-
-if [ -n "$commit_message" ]; then
-    echo "Suggested commit message: $commit_message"
-    echo "Press ENTER to use the suggested commit message, or type a custom one and press ENTER:"
-    read user_input
-
-    if [ -z "$user_input" ]; then
-        user_input="$commit_message"
-    fi
-fi
+      if [ -z "$user_input" ]; then
+          user_input="$commit_message"
+      fi
+  fi
+fi  
 
 git commit -m "$user_input"
--- gpt_commit_message.py
+++ gpt_commit_message.py
@@ -6,13 +6,28 @@ from requests.exceptions import RequestException
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
-def generate_commit_message(changes):
-    prompt = f"Generate a commit message for the following changes in a software project:\n\n{changes}\n\nCommit message: "
+def generate_summary(file_path):
+    with open(file_path, 'r') as f:
+        contents = f.read()
+
+    # Extract summary from contents
+    summary = contents.split('\n')[0]
+    return summary
+
+def generate_commit_message(file_paths):
+    prompts = []
+    for file_path in file_paths:
+        prompt = f"Change in file: {file_path}\n"
+        summary = generate_summary(file_path)
+        prompt += f"Summary: {summary}\n"
+        prompts.append(prompt)
+
+    prompt_text = ''.join(prompts)
 
     try:
         response = openai.Completion.create(
             engine="text-davinci-003",
-            prompt=prompt,
+            prompt=f"Generate a commit message for the following changes:\n{prompt_text}",
             max_tokens=60,
             n=1,
             stop=None,
